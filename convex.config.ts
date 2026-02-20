e8a4ca96d2da1714f692b3fc05bd1d2d5d71a00a
楊閎 <yanghong@yanghongdeMacBook-Air.local>
2026-02-20 16:09:37 +0700

    Fix build - remove convex twilio

--- convex.config.ts
+++ convex.config.ts
@@ -1,14 +1,2 @@
-import { defineApp } from "convex/server";
-import twilio from "twilio";
-
-const app = defineApp();
-
-app.route(
-  "action:hello",
-  twilio.handler({
-    bodyParser: twilio.bodyParser(),
-    validateRequest: twilio.validateRequest(process.env.TWILIO_AUTH_TOKEN!),
-  })
-);
-
-export default app;
+// Convex configuration - stub for future use
+export default {};
